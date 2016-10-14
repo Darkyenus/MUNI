fca63d343043ec3393de159b7caf15ac7b2bf64c
Darkyenus <darkyenus@gmail.com>
2016-10-14 10:00:38 +0200

    Parallelize JBangSimulator

--- JBangSimulator/src/darkyenus/jbangsimulator/BangMain.java
+++ JBangSimulator/src/darkyenus/jbangsimulator/BangMain.java
@@ -4,6 +4,9 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static darkyenus.jbangsimulator.BangSimulator.*;
 
@@ -17,7 +20,7 @@ public class BangMain {
         final Map<AbstractBrain, Integer> scores = new HashMap<>();
         final List<AbstractBrain[]> fightPairs = new ArrayList<>();
 
-        for (int brain1 = 0; brain1 < brains.length-1; brain1++) {
+        for (int brain1 = 0; brain1 < brains.length; brain1++) {
             for (int brain2 = brain1+1; brain2 < brains.length; brain2++) {
                 fightPairs.add(new AbstractBrain[]{brains[brain1], brains[brain2]});
                 fightPairs.add(new AbstractBrain[]{brains[brain2], brains[brain1]});
@@ -41,8 +44,47 @@ public class BangMain {
                 .forEachOrdered(entry -> System.out.println(entry.getKey()+" with "+entry.getValue()+" victories"));
     }
 
+    public static void compareParallel(int runs, boolean log, AbstractBrain...brains) {
+        final long startTime = System.currentTimeMillis();
+        final Map<AbstractBrain, AtomicInteger> scores = new HashMap<>();
+        final List<AbstractBrain[]> fightPairs = new ArrayList<>();
+
+        for (int brain1 = 0; brain1 < brains.length; brain1++) {
+            scores.put(brains[brain1], new AtomicInteger(0));
+            for (int brain2 = brain1+1; brain2 < brains.length; brain2++) {
+                fightPairs.add(new AbstractBrain[]{brains[brain1], brains[brain2]});
+                fightPairs.add(new AbstractBrain[]{brains[brain2], brains[brain1]});
+            }
+        }
+
+        final ForkJoinPool pool = new ForkJoinPool(Runtime.getRuntime().availableProcessors()-1);
+
+        for (AbstractBrain[] fightPair : fightPairs) {
+            for (int run = 0; run < runs; run++) {
+                final int runSeed = run;
+                pool.execute(() -> {
+                    final Player winner = simulateGame(new Game(log, runSeed, fightPair[0], fightPair[1]));
+                    if(winner != null) {
+                        scores.get(winner.brain).incrementAndGet();
+                    }
+                });
+
+            }
+        }
+        if (!pool.awaitQuiescence(5, TimeUnit.MINUTES)) {
+            System.err.println("Timed out");
+        }
+
+        final float elapsedTime = (System.currentTimeMillis() - startTime) /1000f;
+        System.out.println("Simulated "+(fightPairs.size() * runs)+" runs in "+(elapsedTime)+" seconds ("+((fightPairs.size() * runs)/elapsedTime)+" runs per second)");
+
+        scores.entrySet().stream()
+                .sorted((entry1, entry2) -> entry1.getValue().get() - entry2.getValue().get())
+                .forEachOrdered(entry -> System.out.println(entry.getKey()+" with "+entry.getValue().get()+" victories"));
+    }
+
     public static void main(String[] args){
-        compare(10_000, false,
+        compareParallel(10_000, false,
                 new BangBrains.CustomisablyResponsiveBrain(),
                 new BangBrains.CustomisablyResponsiveBrain("HealingLater",
                         BangBrains.CustomisablyResponsiveBrain::actionOpenBoxes,
