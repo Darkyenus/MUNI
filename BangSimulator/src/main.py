1c59bf4d1f2821403db91fd8dc71861c6238163a
Darkyenus <darkyenus@gmail.com>
2016-10-21 16:51:13 +0200

    Further improvements on launch script

--- BangSimulator/src/main.py
+++ BangSimulator/src/main.py
@@ -4,8 +4,8 @@ from game_bang_brains import *
 
 MODE_MANUAL_FIGHT = 1
 MODE_DEBUG_BREEDING = 2
-MODE_QUICK_BREEDING = 2
-MODE_FULL_BREEDING = 3
+MODE_QUICK_BREEDING = 3
+MODE_FULL_BREEDING = 4
 
 bred_brain = None
 
@@ -15,12 +15,13 @@ def start(mode):
         compare_brains(1, True, 0, HumanBrain(), bred_brain if bred_brain is not None else CustomisablyResponsiveBrain(name="DefaultOpponent"))
     elif mode == MODE_DEBUG_BREEDING:
         bred_brain = start_genetic_breeding(fights_per_match=20, iterations=10)
-    elif mode == MODE_FULL_BREEDING:
-        bred_brain = start_genetic_breeding(fights_per_match=80, iterations=40)
-    elif mode == MODE_FULL_BREEDING:
+    elif mode == MODE_QUICK_BREEDING:
         bred_brain = start_genetic_breeding(fights_per_match=100, iterations=100)
+    elif mode == MODE_FULL_BREEDING:
+        bred_brain = start_genetic_breeding(fights_per_match=100, iterations=1000)
     else:
         print("Invalid mode")
 
+#start(MODE_QUICK_BREEDING)
 start(MODE_QUICK_BREEDING)
 start(MODE_MANUAL_FIGHT)
